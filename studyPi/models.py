9c64f6989c51a1e815908754ac732c5518cdfe38
moffy-Black <black.river.emperor@i.softbank.jp>
2020-09-10 16:41:56 +0900

    fix db and add name&localId

--- studyPi/models.py
+++ studyPi/models.py
@@ -3,11 +3,11 @@ from studyPi import db
 class User(db.Model):
   __tablename__ = 'users'
   id = db.Column(db.Integer, primary_key=True)
-  email = db.Column(db.String(100), unique=True, nullable=False)
-  password = db.Column(db.String(100), nullable=False)
+  local_id = db.Column(db.String(100), nullable=False)
+  name = db.Column(db.String(100), nullable=False)
 
   def __repr__(self):
-    return '<User id={id} email={email!r}>'.format(
-      id=self.id, email=self.email)
+    return '<User id={id} name={name!r}>'.format(
+      id=self.id, name=self.name)
 def init():
   db.create_all()
