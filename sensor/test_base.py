56206efd989dea1474ac758cd33a429d72e937f3
moffy-Black <black.river.emperor@i.softbank.jp>
2020-09-15 21:47:29 +0900

    fix c.execute

--- sensor/test_base.py
+++ sensor/test_base.py
@@ -6,7 +6,7 @@ import json
 import sqlite3
 
 with open("./firebaseConfig.json") as f:
-    firebaseConfig = json.loads(f.read())
+  firebaseConfig = json.loads(f.read())
 firebase = pyrebase.initialize_app(firebaseConfig)
 
 db = firebase.database()
@@ -16,6 +16,7 @@ catch = datetime.now()
 release = datetime.now()
 T = 0
 d = 0
+flag = True
 
 # GPIO_sensor
 GPIO_PIN = 18
@@ -25,36 +26,40 @@ GPIO.setup(GPIO_PIN,GPIO.IN)
 # connect flask_db
 conn = sqlite3.connect(r'/home/moffy/enviroment/flask/studyPi/studyPi.db')
 c = conn.cursor()
+c.execute("select * from users")
 db_list = c.fetchone()
 user_id = db_list[1]
 
 
 if __name__ == '__main__':
-  try:
-    while True:
-      if(GPIO.input(GPIO_PIN) == GPIO.HIGH):
-        catch = datetime.now()
-        T += d
-        d = 0
-      else:
-        release = datetime.now()
-        DELTA = release - catch
-        d = DELTA.total_seconds()
-        if d >= 20.0:
-          break
-  except KeyboardInterrupt:
-    pass
-    
-  finally:
-    s = T
-    date = release.strftime('%Y-%m-%d')
-    term = s // 60
-    time = release.strftime('%H:%M')
-    push_date = {
-      "date": date,
-      "term": term,
-      "time": time
-    }
-    records = db.child("records").child(user_id).push(push_date)
-    GPIO.cleanup()
-    print(T)
+  while flag:
+    try:
+      while True:
+        if(GPIO.input(GPIO_PIN) == GPIO.HIGH):
+          catch = datetime.now()
+          T += d
+          d = 0
+        else:
+          release = datetime.now()
+          DELTA = release - catch
+          d = DELTA.total_seconds()
+          if d >= 20.0:
+            break
+    except KeyboardInterrupt:
+      flag = False
+      
+    finally:
+      s = T
+      date = release.strftime('%Y-%m-%d')
+      term = s // 60
+      time = release.strftime('%H:%M')
+      push_date = {
+        "date": date,
+        "term": term,
+        "time": time
+      }
+      records = db.child("records").child(user_id).push(push_date)
+      d = 0
+  conn.close()
+  GPIO.cleanup()
+
