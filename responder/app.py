43b69311496eb05972cbe0bd09ae1f90d05189ae
moffy-Black <black.river.emperor@i.softbank.jp>
2020-10-02 02:14:55 +0900

    + responder

---
+++ responder/app.py
@@ -0,0 +1,82 @@
+import responder
+import os
+import pyrebase
+import json
+import sqlite3
+
+from store import insert_data, remove_data
+from sensor import sensor_on
+
+with open(("firebaseConfig.json")) as f:
+    firebaseConfig = json.loads(f.read())
+firebase = pyrebase.initialize_app(firebaseConfig)
+
+auth = firebase.auth()
+
+api = responder.API(
+  templates_dir='templates',
+  static_dir='static',
+  static_route='/static',
+  secret_key=os.urandom(24)
+)
+
+@api.route('/')
+async def index(req, resp):
+  displayName = req.session.get('displayName')
+  if not displayName:
+    api.redirect(resp,'/login')
+  sensor()
+  resp.content = api.template('index.html',name=displayName)
+
+@api.route('/login')
+async def login(req, resp):
+  if req.method == 'get':
+    resp.content = api.template('login.html',msg="")
+    return
+  else:
+    data = await req.media()
+    email = data.get('email')
+    password = data.get('password')
+    try:
+      # ---Firebase's Function---
+      user = auth.sign_in_with_email_and_password(email, password)
+      localId=user["localId"]
+      name=user["displayName"]
+      insert_data(localId, name)
+      resp.session['displayName']=name
+      api.redirect(resp, '/')
+    except:
+      resp.content = api.template('login.html',msg="メールアドレスまたはパスワードが間違っています。")
+  
+@api.route('/logout')
+def logout(req, resp):
+  remove_data()  
+  api.redirect(resp,'/login')
+
+@api.background.task
+def sensor():
+  sensor_on()
+# class Login:
+#   def url_get(self, req, resp):
+#     if req.method == 'get':
+#       resp.content = api.template('login.html',msg="")
+#     else:
+#       data = await req.media()
+#       email = data.get('email')
+#       password = data.get('password')
+#       try:
+#         # ---Firebase's Function---
+#         user = auth.sign_in_with_email_and_password(email, password)
+#         localId=user["localId"],
+#         name=user["displayName"]
+#         insert_data(localId, name)
+#         resp.session['displayName']=name
+#         api.redirect(resp, '/')
+#       except:
+#         resp.content = api.template('login.html',msg="メールアドレスまたはパスワードが間違っています。")
+
+# api.add_route("/login", Login)
+# api.add_route("/", Index)
+
+if __name__ == "__main__":
+  api.run(address="0.0.0.0",port=5050, debug=True)
---
+++ responder/db.py
@@ -0,0 +1,14 @@
+
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+Base = declarative_base()
+DB_PATH = 'sqlite:///studyPi.db'
+ECHO_LOG = False
+engine = create_engine(
+   DB_PATH, echo=ECHO_LOG
+)
+
+Session = sessionmaker(bind=engine)
+session = Session()
